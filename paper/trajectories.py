191008850de3414f14a8b2f9bc4849eaf8fe6d4a
glopes <glopesdev@users.noreply.github.com>
2014-09-02 14:13:43 +0100

    Added speed related functions to trajectory analysis.

--- paper/trajectories.py
+++ paper/trajectories.py
@@ -28,25 +28,50 @@ class trajectories:
             slices = np.ma.clump_unmasked(mask)
         self.slices = slices
         
+    def __repr__(self):
+        if self.slices is None:
+            return str.format("trajectories({0})", self.data)
+        else:
+            return str.format("trajectories({0}, {1})", self.data, self.slices)
+        
     def tolist(self):
         return [self.data[s,:] for s in self.slices]
         
-def scale(ts,sx=width_pixel_to_cm,sy=height_pixel_to_cm,by=rail_height_pixels,my=max_height_cm):
+def scale(ts,
+          sx=width_pixel_to_cm,
+          sy=height_pixel_to_cm,
+          by=rail_height_pixels,
+          my=max_height_cm):
     scaled = [0,my,0,my] - (ts.data + [0,by,0,by]) * [-sx,sy,-sx,sy]
     return trajectories(scaled,ts.slices)
     
 def crossings(ts,center=640):
-    return trajectories(ts.data,[s for s in ts.slices if
+    return trajectories(ts.data,np.array([s for s in ts.slices if
     (ts.data[s.start,0] < center and ts.data[s.stop-1,0] > center) or
-    (ts.data[s.start,0] > center and ts.data[s.stop-1,0] < center)])
+    (ts.data[s.start,0] > center and ts.data[s.stop-1,0] < center)]))
     
 def lengthfilter(ts,minlength=None,maxlength=None):
-    return trajectories(ts.data,[s for s in ts.slices if
-    s.stop-s.start >= minlength and (maxlength is None or s.stop-s.start <= maxlength)])
+    return trajectories(ts.data,np.array([s for s in ts.slices if
+    s.stop-s.start >= minlength and
+    (maxlength is None or s.stop-s.start <= maxlength)]))
     
 def heightfilter(ts,minheight=None,maxheight=None):
-    return trajectories(ts.data,[s for s in ts.slices if
-    min(ts.data[s,1]) > minheight and max(ts.data[s,1]) < maxheight])
+    return trajectories(ts.data,np.array([s for s in ts.slices if
+    min(ts.data[s,1]) > minheight and max(ts.data[s,1]) < maxheight]))
+    
+def speed(ts,time):
+    timedelta = np.diff(time)
+    deltaT = np.tile(timedelta,(ts.data.shape[1],1)).T
+    speed = np.insert(np.diff(ts.data,axis=0) / deltaT,0,0,axis=0)
+    return trajectories(speed,ts.slices)
+    
+def speedbins(ts,sp,bins=100):
+    def computebins(s):
+        weights = sp.data[s,0] if s.start < s.stop else -sp.data[s,0]
+        binsums = np.histogram(ts.data[s,0],bins,weights=weights)[0]
+        bincounts = np.histogram(ts.data[s,0],bins,weights=None)[0]
+        return binsums / bincounts
+    return [computebins(s) for s in ts.slices]
     
 def crop(ts,crop=[200,1000]):
     def test_slice(s):
@@ -57,25 +82,25 @@ def crop(ts,crop=[200,1000]):
         min_index = np.min(valid_indices)
         max_index = np.max(valid_indices)
         return slice(s.start+min_index,s.start+max_index+1)
-    return trajectories(ts.data,[crop_slice(s) for s in ts.slices
-    if np.any(test_slice(s))])
+    return trajectories(ts.data,np.array([crop_slice(s) for s in ts.slices
+    if np.any(test_slice(s))]))
 
 def mirrorleft(ts):
-    return trajectories(ts.data,[slice(s.stop,s.start,-1)
+    return trajectories(ts.data,np.array([slice(s.stop,s.start,-1)
     if ts.data[s.start,0] > ts.data[s.stop,0] else s
-    for s in ts.slices])
+    for s in ts.slices]))
         
 def left(ts):
-    return trajectories(ts.data,[s for s in ts.slices
-    if ts.data[s.start,0] > ts.data[s.stop,0]])
+    return trajectories(ts.data,np.array([s for s in ts.slices
+    if ts.data[s.start,0] > ts.data[s.stop,0]]))
         
 def right(ts):
-    return trajectories(ts.data,[s for s in ts.slices
-    if ts.data[s.start,0] < ts.data[s.stop,0]])
+    return trajectories(ts.data,np.array([s for s in ts.slices
+    if ts.data[s.start,0] < ts.data[s.stop,0]]))
         
 def crossindices(ts,center=25.0):
-    return np.array([next(i+s.start for i,x in enumerate(ts.data[s,0]) if x < center)
-    for s in ts.slices[1:]])
+    return np.array([next(i+s.start for i,x in enumerate(ts.data[s,0]) if
+    x < center) for s in ts.slices[1:]])
         
 def genfromtxt(path):
     trajectoriespath = os.path.join(path, 'Analysis/trajectories.csv')
