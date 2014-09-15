91cb8360e5b4a6354e5d7dfad9e8d651cc8a3037
glopes <glopesdev@users.noreply.github.com>
2014-09-15 15:03:13 +0100

    Added support for pandas preprocessing.

--- paper/preprocess.py
+++ paper/preprocess.py
@@ -12,10 +12,30 @@ import filecmp
 import dateutil
 import subprocess
 import numpy as np
+import pandas as pd
 
 abspath = os.path.abspath(__file__)
 dname = os.path.dirname(abspath)
 
+fronttime_key = 'video/front/time'
+frontactivity_key = 'video/front/activity'
+toptime_key = 'video/top/time'
+whiskertime_key = 'video/whisker/time'
+leftpoke_key = 'task/poke/left/activity'
+rightpoke_key = 'task/poke/right/activity'
+rewards_key = 'task/rewards'
+
+max_height_cm = 24.0
+max_width_cm = 50.0
+height_pixel_to_cm = max_height_cm / 680.0
+width_pixel_to_cm = max_width_cm / 1280.0
+rail_height_pixels = 100
+rail_start_pixels = 200
+rail_stop_pixels = 1000
+frames_per_second = 120.0
+
+h5filename = 'session.hdf5'
+labelh5filename = 'labels.hdf5'
 analysisfolder = 'Analysis'
 backgroundfolder = 'Background'
 playerpath = os.path.join(dname, r'../bonsai.lesions/Bonsai.Player.exe')
@@ -54,6 +74,160 @@ def process_sessions(datafolders,preprocessing=True,overwrite=None):
     for path in datafolders:
         analysispath = os.path.join(path,analysisfolder)
         make_videoanalysis(analysispath)
+        
+    print "Generating datasets..."
+    for path in datafolders:
+        print "Generating dataset for "+ path + "..."
+        createdataset(path,overwrite=True)
+        
+def storepath(path):
+    return os.path.join(path, analysisfolder, h5filename)
+    
+def labelpath(path):
+    return os.path.join(path, analysisfolder, labelh5filename)
+
+def readtimestamps(path):
+    timestamps = pd.read_csv(path,header=None,names=['time'])
+    return pd.to_datetime(timestamps['time'])
+    
+def scaletrajectories(ts,
+          sx=width_pixel_to_cm,
+          sy=height_pixel_to_cm,
+          by=rail_height_pixels,
+          my=max_height_cm):
+    return [0,my,0,my] - (ts + [0,by,0,by]) * [-sx,sy,-sx,sy]
+    
+def sliceframe(slices):
+    return pd.DataFrame([(s.start,s.stop) for s in slices],
+                        columns=['start','stop'])
+    
+def indexseries(series,index):
+    diff = len(index) - len(series)
+    if diff > 0:
+        msg="WARNING: time series length smaller than index by {0}. Padding..."
+        print str.format(msg,diff)
+        lastrow = series.tail(1)
+        series = series.append([lastrow] * diff)
+    series.index = index
+    return series
+    
+def readpoke(path):
+    return pd.read_csv(path,
+                       sep=' ',
+                       header=None,
+                       names=['activity','time'],
+                       dtype={'activity':np.int32,'time':pd.datetime},
+                       parse_dates=[1],
+                       index_col=1,
+                       usecols=[0,1])
+                       
+def readstep(path,name):
+    return pd.read_csv(path,
+                       header=None,
+                       true_values=['True'],
+                       false_values=['False'],
+                       names=[name])[name]
+        
+def createdataset(path,overwrite=False):
+    h5path = storepath(path)
+    if os.path.exists(h5path):
+        if overwrite:
+            print "Overwriting..."
+            os.remove(h5path)
+        else:
+            print "Skipped!"
+            return
+
+    # Load raw data    
+    fronttime = readtimestamps(os.path.join(path, 'front_video.csv'))
+    toptime = readtimestamps(os.path.join(path, 'top_video.csv'))
+    whiskertime = readtimestamps(os.path.join(path, 'whisker_video.csv'))
+    leftrewards = readtimestamps(os.path.join(path, 'left_rewards.csv'))
+    rightrewards = readtimestamps(os.path.join(path, 'right_rewards.csv'))
+    leftpoke = readpoke(os.path.join(path, 'left_poke.csv'))
+    rightpoke = readpoke(os.path.join(path, 'right_poke.csv'))
+    
+    # Load preprocessed data
+    trajectorypath = os.path.join(path, analysisfolder, 'trajectories.csv')
+    stepactivitypath = os.path.join(path, analysisfolder, 'step_activity.csv')
+    gapactivitypath = os.path.join(path, analysisfolder, 'slip_activity.csv')
+    trajectories = pd.read_csv(trajectorypath,
+                               sep = ' ',
+                               header=None,
+                               dtype=np.int32,
+                               names=['xhead','yhead','xtail','ytail'])
+    stepactivity = pd.read_csv(stepactivitypath,
+                               sep = ' ',
+                               header = None,
+                               dtype=np.int32,
+                               names=[str.format('stepactivity{0}',i)
+                               for i in xrange(8)])
+    gapactivity = pd.read_csv(gapactivitypath,
+                              sep = ' ',
+                              header = None,
+                              dtype=np.int32,
+                              names=[str.format('gapactivity{0}',i)
+                              for i in xrange(7)])
+    trajectories = indexseries(trajectories,fronttime)
+    scaledtrajectories = scaletrajectories(trajectories[trajectories >= 0])
+    trajectories[trajectories < 0] = np.NaN
+    trajectories[trajectories >= 0] = scaledtrajectories
+    stepactivity = indexseries(stepactivity,fronttime)
+    gapactivity = indexseries(gapactivity,fronttime)
+    
+    # Compute speed
+    speed = trajectories.diff()
+    timedelta = pd.DataFrame(fronttime.diff() / np.timedelta64(1,'s'))
+    timedelta.index = speed.index
+    speed = pd.concat([speed,timedelta],axis=1)
+    speed = speed.div(speed.time,axis='index')    
+    speed.columns = ['xhead_speed',
+                     'yhead_speed',
+                     'xtail_speed',
+                     'ytail_speed',
+                     'timedelta']
+    speed['timedelta'] = timedelta
+    
+    # Compute reward times
+    leftrewards = pd.DataFrame(leftrewards)
+    rightrewards = pd.DataFrame(rightrewards)
+    leftrewards['side'] = 'left'
+    rightrewards['side'] = 'right'
+    rewards = pd.concat([leftrewards,rightrewards])
+    rewards.sort(columns=['time'],inplace=True)
+    
+    # Compute trial indices and environment state
+    trialindex = pd.concat([fronttime[0:1],rewards.time])
+    trialseries = pd.Series(range(len(trialindex)),
+                            dtype=np.int32,
+                            name='trial')
+    if len(trialindex) > 200:
+        print "WARNING: Trial count exceeded 200!"
+    steppath = os.path.join(path, 'step{0}_trials.csv')
+    axisname = 'stepstate{0}'
+    stepstates = [readstep(str.format(steppath,i),str.format(axisname,i))
+    for i in xrange(1,7)]
+    trialseries = pd.concat([trialseries] + stepstates,axis=1)
+    trialseries.fillna(method='ffill',inplace=True)
+    trialseries = trialseries[0:len(trialindex)]
+    trialseries.index = trialindex
+    trialseries = trialseries.reindex(fronttime,method='ffill')
+    
+    # Generate big data table
+    frontactivity = pd.concat([trialseries,
+                               trajectories,
+                               speed,
+                               stepactivity,
+                               gapactivity],
+                               axis=1)
+    
+    fronttime.to_hdf(h5path, fronttime_key)
+    frontactivity.to_hdf(h5path, frontactivity_key)
+    toptime.to_hdf(h5path, toptime_key)
+    whiskertime.to_hdf(h5path, whiskertime_key)
+    leftpoke.to_hdf(h5path, leftpoke_key)
+    rightpoke.to_hdf(h5path, rightpoke_key)
+    rewards.to_hdf(h5path, rewards_key)
 
 def sessionlabel(path):
     protocolfilefolder = os.path.join(dname,'../protocolfiles/lesionsham')
