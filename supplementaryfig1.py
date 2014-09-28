85645aebac2d09bdeca6b356407ac8ebe832e9a8
glopes <glopesdev@users.noreply.github.com>
2014-09-28 10:58:52 +0100

    Updated lesion data preprocessing in order to generate pandas dataset.

--- supplementaryfig1.py
+++ supplementaryfig1.py
@@ -6,15 +6,18 @@ Created on Wed Oct 02 17:50:21 2013
 """
 
 import os
+import pandas as pd
 from process_rois import *
 
 output_path = r'C:/figs'
-generate_figs = True
+generate_figs = False
 
 # Volume statistics #
 barlabels = []
+subjects = []
 left_volumes = []
 right_volumes = []
+whole_volumes = []
 lesion_order = np.array([0, 4, 12, 10, 2, 18, 8, 14, 20, 16, 6])
 control_order = lesion_order+1
 # JPAK 20,24,38,36,22,52,28,48,54,50,26
@@ -24,6 +27,7 @@ control_order = lesion_order+1
 def load_slices(name,trim_end=None):
     global left_volumes
     global right_volumes
+    global whole_volumes
     whole,references = read_whole_slices(name)
     if trim_end is not None:
         whole = whole[0:trim_slices]
@@ -32,6 +36,7 @@ def load_slices(name,trim_end=None):
     right = read_slice(name,'RightRoiSet',references)
     left_volumes.append(get_volume_rois(left))
     right_volumes.append(get_volume_rois(right))
+    whole_volumes.append(get_volume_rois(whole))
     return whole,left,right
     
 def generate_figures(name,left,right,whole,bregma_228_pos,sliceline_pos=None):
@@ -53,6 +58,7 @@ def generate_figures(name,left,right,whole,bregma_228_pos,sliceline_pos=None):
 ########################### JPAK 20 ###########################################
 name = 'JPAK_20'
 label = 'La'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 33
 whole,left,right = load_slices(name)
@@ -62,6 +68,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 24 ###########################################
 name = 'JPAK_24'
 label = 'Lb'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 32
 whole,left,right = load_slices(name)
@@ -71,6 +78,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 38 ###########################################
 name = 'JPAK_38'
 label = 'Lc'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 29
 whole,left,right = load_slices(name)
@@ -80,6 +88,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 36 ###########################################
 name = 'JPAK_36'
 label = 'Ld'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 35
 sliceline_pos = 36
@@ -90,6 +99,7 @@ generate_figures(label,left,right,whole,bregma_228_pos,sliceline_pos)
 ########################### JPAK 22 ###########################################
 name = 'JPAK_22'
 label = 'Le'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 37
 whole,left,right = load_slices(name)
@@ -99,6 +109,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 52 ###########################################
 name = 'JPAK_52'
 label = 'Lf'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 36
 whole,left,right = load_slices(name)
@@ -108,6 +119,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 28 ###########################################
 name = 'JPAK_28'
 label = 'Lg'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 34
 whole,left,right = load_slices(name)
@@ -117,6 +129,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 48 ###########################################
 name = 'JPAK_48'
 label = 'Lh'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 39
 whole,left,right = load_slices(name)
@@ -126,6 +139,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 54 ###########################################
 name = 'JPAK_54'
 label = 'Li'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 37
 whole,left,right = load_slices(name)
@@ -135,6 +149,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 50 ###########################################
 name = 'JPAK_50'
 label = 'Lj'
+subjects.append(name)
 barlabels.append(label)
 bregma_228_pos = 37
 whole,left,right = load_slices(name)
@@ -144,6 +159,7 @@ generate_figures(label,left,right,whole,bregma_228_pos)
 ########################### JPAK 26 ###########################################
 name = 'JPAK_26'
 label = 'Lk'
+subjects.append(name)
 barlabels.append(label)
 trim_slices = 55
 bregma_228_pos = 32
@@ -155,14 +171,21 @@ generate_figures(label,left,right,whole,bregma_228_pos,sliceline_pos)
 ########################### SUMMARY ###########################################
 barwidth = 0.35
 barticks = np.arange(len(left_volumes))
+#left_volumes = np.array([l/w for l,w in zip(left_volumes,whole_volumes)])
+#right_volumes = np.array([r/w for r,w in zip(right_volumes,whole_volumes)])
 fig = plt.figure()
 ax = plt.gca()
 r1 = ax.bar(barticks,right_volumes,barwidth,color='orange')
 r2 = ax.bar(barticks+barwidth,left_volumes,barwidth,color='purple')
+#r3 = ax.bar(barticks+barwidth*2,whole_volumes,barwidth,color='gray')
 ax.legend((r1[0],r2[0]),('left','right'))
 ax.set_xticks(barticks+barwidth)
 ax.set_xticklabels(barlabels)
 plt.ylabel('lesion volume (mm$^3$)')
-plt.savefig(os.path.join(output_path,'lesion_summary.pdf'))
-plt.close(fig)
+#plt.savefig(os.path.join(output_path,'lesion_summary.pdf'))
+dataset = pd.DataFrame(np.vstack((right_volumes,left_volumes)).T,
+                       index=pd.Index(subjects,name='subject'),
+                       columns=['lesion_left','lesion_right'])
+dataset.to_csv(os.path.join(output_path, 'lesion_volumes.csv'))
+#plt.close(fig)
 ###############################################################################
