864b83ffa6544b53c20ef66ecee4aaec6c97017e
glopes <glopesdev@users.noreply.github.com>
2015-04-30 15:29:56 +0100

    Added datapath module to encapsulate common analysis storage paths.

---
+++ paper/datapath.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 11:20:49 2015
+
+@author: Gonçalo
+"""
+
+lesionsham = [r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_20',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_21',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_22',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_23',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_24',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_25',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_26',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_27',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_28',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_29',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_36',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_37',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_38',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_39',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_48',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_49',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_50',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_51',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_52',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_53',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_54',
+              r'D:/Protocols/Shuttling/LightDarkServoStable/Data/JPAK_55']
+            
+decorticates = [r'D:/Protocols/Shuttling/Decorticate/Data/JPAK_79',
+                r'D:/Protocols/Shuttling/Decorticate/Data/JPAK_81',
+                r'D:/Protocols/Shuttling/Decorticate/Data/JPAK_82']
+                
+decorticatecache = r'D:/Protocols/Shuttling/Decorticate/Analysis/cache.hdf5'
+lesionshamcache = r'D:/Protocols/Shuttling/LightDarkServoStable/Analysis/cache.hdf5'
+visiblecrossings_key = 'visiblecrossings'
+fullcrossings_key = 'fullcrossings'
+crossings_key = 'crossings'
