a7b77f24036d34e7b0fa804f001fb5c0a357c5f3
glopes <glopesdev@users.noreply.github.com>
2015-04-30 15:30:51 +0100

    Added crossingcache module for precomputing heavy-lifted analysis routines.

---
+++ paper/crossingcache.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 30 12:30:38 2015
+
+@author: Gonçalo
+"""
+
+from activitytables import read_subjects
+from activitytables import visiblecrossings, fullcrossings, crossings
+from datapath import lesionsham, decorticates
+from datapath import lesionshamcache, decorticatecache
+from datapath import visiblecrossings_key, fullcrossings_key, crossings_key
+
+# Rebuild crossing cache
+print "Rebuilding lesionsham visible crossings..."
+cr = read_subjects(lesionsham,selector=visiblecrossings)
+cr.to_hdf(lesionshamcache,visiblecrossings_key)
+
+print "Rebuilding lesionsham full crossings..."
+cr = read_subjects(lesionsham,selector=fullcrossings)
+cr.to_hdf(lesionshamcache,fullcrossings_key)
+
+print "Rebuilding lesionsham crossings..."
+cr = read_subjects(lesionsham,selector=crossings)
+cr.to_hdf(lesionshamcache,crossings_key)
+
+print "Rebuilding decorticates visible crossings..."
+cr = read_subjects(decorticates,selector=visiblecrossings)
+cr.to_hdf(decorticatecache,visiblecrossings_key)
+
+print "Rebuilding decorticates full crossings..."
+cr = read_subjects(decorticates,selector=fullcrossings)
+cr.to_hdf(decorticatecache,fullcrossings_key)
+
+print "Rebuilding decorticates crossings..."
+cr = read_subjects(decorticates,selector=crossings)
+cr.to_hdf(decorticatecache,crossings_key)
