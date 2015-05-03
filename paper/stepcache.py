b38f56d38be9e421e5b8b15fb1c7a95941e246c0
glopes <glopesdev@users.noreply.github.com>
2015-05-03 19:03:25 +0100

    Added step feature cache to avoid recomputing center stepping information.

---
+++ paper/stepcache.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun May 03 15:08:56 2015
+
+@author: Gonçalo
+"""
+
+from activitytables import read_subjects
+from activitytables import stepfeatures
+from datapath import lesionsham, decorticate
+from datapath import lesionshamcache, decorticatecache
+from datapath import stepfeatures_key
+
+# Rebuild crossing cache
+print "Rebuilding lesionsham step features..."
+cr = read_subjects(lesionsham,selector=stepfeatures)
+cr.to_hdf(lesionshamcache,stepfeatures_key)
+
+print "Rebuilding decorticate visible crossings..."
+cr = read_subjects(decorticate,selector=stepfeatures)
+cr.to_hdf(decorticatecache,stepfeatures_key)
